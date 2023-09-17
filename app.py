a30b5bfe7aa91bf1ecb4a22356aca172036a64b3
yongsean <88657089+yongsean@users.noreply.github.com>
2023-09-17 11:08:17 +0800

    a

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 from flask import render_template, make_response
 from flask import redirect
 import mimetypes
-from flask import Flask, render_template, request, redirect, url_for, session, send_file
+from flask import Flask, render_template, request, redirect, url_for, session, send_file, jsonify
 from botocore.exceptions import ClientError
 from pymysql import connections
 import boto3
