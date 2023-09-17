891f2d12de6c89ebc1655ded795b0ad045636b80
yongsean <88657089+yongsean@users.noreply.github.com>
2023-09-17 12:20:24 +0800

    Update app.py

--- app.py
+++ app.py
@@ -1646,9 +1646,11 @@ def displayAllJobs():
                     salary = job[8]
                     company_id = job[10]
                     company_name = job[12]  # Extracted from the JOINed column
-            
+
+                    
                     # Generate the S3 image URL using custombucket and customregion
-                    company_image_file_name_in_s3 = "company/"+"comp-id-" + str(company_id) + "_image_file"
+                    company_image_file_name_in_s3 = "comp-id-" + \
+                        str(company_id)+"_image_file"
                     s3 = boto3.client('s3', region_name=customregion)
                     bucket_name = custombucket
 
