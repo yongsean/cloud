cb7b0cf4bb432c3ef461de19091832721821f733
Kl0603 <lokekok47@gmail.com>
2023-09-24 02:09:08 +0800

    a

--- config.py
+++ config.py
@@ -1,4 +1,4 @@
-customhost = "employee.cbffvuek6hel.us-east-1.rds.amazonaws.com"
+customhost = "database-1.cdsilkd4knvu.us-east-1.rds.amazonaws.com"
 customuser = "aws_user"
 custompass = "Bait3273"
 customdb = "employee"
