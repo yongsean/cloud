8d78ac816335562a64ae96a3ed44eb1b8dac19da
Kl0603 <lokekok47@gmail.com>
2023-09-24 02:01:32 +0800

    a

--- config.py
+++ config.py
@@ -1,6 +1,6 @@
-customhost = "database-1.cdsilkd4knvu.us-east-1.rds.amazonaws.com"
+customhost = "employee.cbffvuek6hel.us-east-1.rds.amazonaws.com"
 customuser = "aws_user"
 custompass = "Bait3273"
 customdb = "employee"
-custombucket = "gweeyongsean-bucket"
+custombucket = "lokekoklam-employee1"
 customregion = "us-east-1"
